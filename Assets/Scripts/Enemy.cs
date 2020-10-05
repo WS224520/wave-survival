124e42ad3c209c3a19a28177e331758beb59725f
WS224520 <ws224520@weston.ac.uk>
2020-10-05 14:27:37 +0100

    Enemy Error Fix
    -Fixed an error that crashed when a second enemy is killed

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -78,7 +78,7 @@ public class Enemy : MonoBehaviour
         Destroy(this.gameObject);
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
+    public void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
